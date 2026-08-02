035b4ac0efa1dcba627322c18ff615ed75f5c079
GJ Tiquia <gjtiquia@gmail.com>
2026-08-02 02:59:58 -0400

    add TODOs

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,8 @@
 
+// TODO : thinking... with the web having so many "quirks", it might be better to emulate browser behavior in TUI, than stripping down browser to TUI
+
+// TODO : hoping to decouple to a point that its "backend agnostic", at least the frontend parts!
+
 export async function mainAsync(port: number): Promise<Error | null> {
     console.log("TODO: WEB, port", port)
     return null
