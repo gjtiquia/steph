cb3b55c959755712a6e8d5bbdd9f8e7097936267
GJ Tiquia <gjtiquia@gmail.com>
2026-08-08 23:57:24 -0400

    cleanup

--- src/tui/index.ts
+++ src/tui/index.ts
@@ -1,7 +1,5 @@
 import * as tea from "./lib/lemontea"
-import * as t from "./lib/simple-tui"
 import { createRootModel } from "./model"
-import * as readline from "node:readline"
 
 export async function mainAsync(): Promise<Error[] | null> {
     const model = createRootModel()
