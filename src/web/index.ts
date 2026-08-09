c98403c62236ae656c1bc02fa5b97c07650ec2d9
GJ Tiquia <gjtiquia@gmail.com>
2026-08-09 01:00:42 -0400

    setup elysia

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,8 +1,19 @@
+import { Elysia } from "elysia";
+
 // TODO : thinking... with the web having so many "quirks", it might be better to emulate browser behavior in TUI, than stripping down browser to TUI
 
 // TODO : hoping to decouple to a point that its "backend agnostic", at least the frontend parts!
 
 export async function mainAsync(port: number): Promise<Error[] | null> {
-    console.log("TODO: WEB, port", port);
-    return null;
+    const app = new Elysia()
+        .get("/", () => "Hello Elysia")
+        .get("/error", () => {throw new Error("forced elysia error")})
+        .listen(port);
+
+    console.log(
+        `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+    );
+
+    // server process now owns process exit and error logging
+    return null
 }
