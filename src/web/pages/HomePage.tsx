0b8ced390b19a77b80b6eaa189cc83edf0e669c2
GJ Tiquia <gjtiquia@gmail.com>
2026-08-09 23:03:07 -0400

    refactor templates

--- src/web/pages/HomePage.tsx
+++ src/web/pages/HomePage.tsx
@@ -16,36 +16,46 @@ export function HomePage() {
                     href={`public/styles.css?v=${version}`}
                     rel="stylesheet"
                 />
-                <script type="module" src={`/public/index.js?v=${version}`}></script>
+                <script
+                    type="module"
+                    src={`/public/index.js?v=${version}`}
+                ></script>
                 <title>steph</title>
             </head>
             <body class="bg-stone-900 text-stone-50 font-fira">
                 <div id="root" class="flex flex-col gap-2 p-4"></div>
-
-                <template id="tpl-text">
-                    <p class="min-h-[1.5rem] whitespace-pre"></p>
-                </template>
-                <template id="tpl-section">
-                    <div class="flex flex-col gap-1" data-children></div>
-                </template>
-                <template id="tpl-list">
-                    <div class="flex flex-col gap-1">
-                        <h2 class="text-lg" data-title></h2>
-                        <ul class="flex flex-col" data-items></ul>
-                    </div>
-                </template>
-                <template id="tpl-list-item">
-                    <li class="cursor-pointer px-1 rounded-sm"></li>
-                </template>
-                <template id="tpl-widget-input">
-                    <label data-prefix></label>
-                    <input
-                        type="text"
-                        data-input
-                        class="border-1 border-stone-50/25 rounded-sm px-1"
-                    />
-                </template>
+                <Templates />
             </body>
         </html>
+    );
+}
+
+function Templates() {
+    return (
+        <>
+            <template id="tpl-text">
+                <p class="min-h-[1.5rem] whitespace-pre"></p>
+            </template>
+            <template id="tpl-section">
+                <div class="flex flex-col gap-1" data-children></div>
+            </template>
+            <template id="tpl-list">
+                <div class="flex flex-col gap-1">
+                    <h2 class="text-lg" data-title></h2>
+                    <ul class="flex flex-col" data-items></ul>
+                </div>
+            </template>
+            <template id="tpl-list-item">
+                <li class="cursor-pointer px-1 rounded-sm"></li>
+            </template>
+            <template id="tpl-widget-input">
+                <label data-prefix></label>
+                <input
+                    type="text"
+                    data-input
+                    class="border-1 border-stone-50/25 rounded-sm px-1"
+                />
+            </template>
+        </>
     );
 }
